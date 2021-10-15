b37d16c61a16bbb11aa5699e5db66a69e1af259a
josh671 <49129783+josh671@users.noreply.github.com>
2021-10-15 13:44:06 -0700

    adding first flutter files from Udemy

---
+++ flutter_complete_guide/lib/main.dart
@@ -0,0 +1,11 @@
+ //Flutter Complete Guide: Building an app from scratch
+ 
+ void mian()  {
+  //main is special function that dart will run 
+  //container code to render app 
+
+  //adding code to bring stuff onto screen 
+  //flutter uses widgets 
+  
+
+}
